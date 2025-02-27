62b9fe8c99baf73b1ddbe71993eafb7954c4d6b5
Alex-B012 <rad15on2014@gmail.com>
2025-02-26 19:30:56 -0500

    Project completed v1.03

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,27 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "netlify.toml",
+          dest: "../build",
+        },
+        {
+          src: "_redirects",
+          dest: "../build",
+        },
+      ],
+    }),
+  ],
+  build: {
+    outDir: "build",
+  },
 });
